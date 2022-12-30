8a98937e6676f4591fe7f2ba6f7bdfb1ba18935c
Roman Mullayev <romkapomka01@gmail.com>
2022-12-30 00:58:39 +0000

    latest changes

--- demo/src/main/java/roman/training/MoviesBatchJob.java
+++ demo/src/main/java/roman/training/MoviesBatchJob.java
@@ -16,8 +16,8 @@ public class MoviesBatchJob {
 		DataSet<Rating> ratings = getRatings(env);
 		DataSet<Movie> movies = getMovies(env);
 
-		DataSet<Tuple2<Long, Double>> averageRating = ratings.groupBy(0).reduceGroup(new MovieAverageRatingGroupReduceFunction());
-		averageRating = averageRating.sortPartition(1, Order.DESCENDING).setParallelism(1);
+		DataSet<Tuple2<Long, Double>> averageRating = ratings.groupBy(0).reduceGroup(new MovieAverageRatingGroupReduceFunction()).setParallelism(4);
+		averageRating = averageRating.sortPartition(1, Order.DESCENDING).setParallelism(4);
 		DataSet<Tuple2<Long, Double>> top10 = averageRating.first(20);
 		
 		DataSet<Tuple3<Long, String, Double>> top10WithName = top10.joinWithHuge(movies).where(0).equalTo(0).map(new MovieNameMapFunction());
